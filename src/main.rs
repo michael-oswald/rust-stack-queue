b08789fdd02faed28a8c01ca51f7cb54624f66ba
maikerukun <goshipcode@gmail.com>
2023-05-05 13:10:29 -0500

    updates to stack function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::collections::VecDeque;
+use std::{thread, time::Duration};
+
 
 fn main() {
     let mut deq = VecDeque::from([-1, 0, 1]);
@@ -8,33 +10,62 @@ fn main() {
 }
 
 fn stack_example () {
-    //TODO: can we have it have pauses so people can see what it looks like?
     println!("Stack example with VecDeque:");
+    println!("What is VecDeque? It's a double ended queue (so works as a stack or queue) \
+    You can push and pop from front or back of the data structure.\n\n");
 
+    println!("Declare Empty mutable stack: let mut stack = VecDeque::new();");
     let mut stack = VecDeque::new();
-    println!("Empty Stack: {:?}", stack);
+    println!("Empty Stack: {:?} \n", stack);
+    sleep();
 
     println!("push some elements onto the stack:");
+
+    println!("stack.push_front(1);");
     stack.push_front(1);
+
+    sleep();
+    println!("current stack: {:?} \n", stack);
+
+    println!("stack.push_front(2);");
     stack.push_front(2);
+
+    sleep();
+    println!("current stack: {:?} \n", stack);
+
+    println!("stack.push_front(3);");
     stack.push_front(3);
+
+    sleep();
+    println!("current stack: {:?} \n", stack);
+
+    println!("stack.push_front(4);");
     stack.push_front(4);
 
-    println!("current stack: {:?}", stack);
+    sleep();
+    println!("current stack: {:?} \n", stack);
 
-    println!("pop an elements off the stack stack.pop_front()");
-    stack.pop_front();
-    println!("current stack: {:?}", stack);
+    println!("pop an elements off the stack stack.pop_front() returns -> {:?}", stack.pop_front());
+    sleep();
+    println!("current stack: {:?}\n", stack);
 
-    println!("pop an elements off the stack stack.pop_front()");
-    stack.pop_front();
-    println!("current stack: {:?}", stack);
+    println!("pop an elements off the stack stack.pop_front() returns -> {:?}", stack.pop_front());
+    sleep();
+    println!("current stack: {:?}\n", stack);
 
-    println!("peek the top of the stack stack.get(0)");
-    println!("{:?}", stack.get(0));
-    println!("current stack: {:?}", stack);
+    println!("peek at the top of the stack stack.get(0) returns -> {:?}", stack.get(0));
+    sleep();
+    println!("current stack (has no changes): {:?}\n", stack);
 
-    println!("clear the stack with: deque.drain(..);");
-    stack.drain(..);
-    println!("current stack: {:?}", stack);
+    println!("clear the stack with: deque.clear();");
+    stack.clear();
+    sleep();
+    println!("current stack: {:?}\n", stack);
+
+    println!("check if stack is empty with stack.is_empty(); -> {:?}", stack.is_empty());
+
+}
+
+fn sleep() { // pause so console output can be followed more easily.
+    thread::sleep(Duration::from_millis(3000));
 }
