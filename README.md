d3ea97e800a883637093d4726885779806775b13
maikerukun <goshipcode@gmail.com>
2023-05-05 21:13:15 -0500

    finishing touches

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
 # rust-stack-queue
 Tutorial on how to use stack and queue in rust
+
+# VecDeque Data Structure
+For stack and queue in this example we'll use the VecDeque data structure provided by Rust by using: `std::collections::VecDeque;`
+
+The docs say that it is a `A double-ended queue`, which means it can insert and remove elements from both ends
+of the structure. Thus, it can be used as a stack or a queue. Here are the [official docs](https://doc.rust-lang.org/stable/std/collections/struct.VecDeque.html)
+
+# TLDR stack and queue in rust
+**Queue**
+```
+// Queue - FIFO structure 
+//import
+use std::collections::VecDeque;
+
+// First define a mutable VecDeque:
+let mut queue = VecDeque::new();
+
+// add elements to the queue:
+queue.push_back(1);
+queue.push_back(2);
+queue.push_back(3);
+queue.push_back(4);
+
+// remove 2 elements:
+queue.pop_back();
+queue.pop_back();
+
+// peek:
+queue.get(queue.len() - 1);
+
+// clear:
+queue.clear();
+
+```
+
+**Stack**
+
+```
+// Stack - LIFO structure 
+//import
+use std::collections::VecDeque;
+
+// First define a mutable VecDeque:
+let mut stack = VecDeque::new();
+
+// add elements to the stack:
+stack.push_front(1);
+stack.push_front(2);
+stack.push_front(3);
+stack.push_front(4);
+
+// remove 2 elements:
+stack.pop_front();
+stack.pop_front();
+
+// peek:
+stack.get(0);
+
+// clear:
+stack.clear();
+
+```
+
+# Run this example:
+Prereq: You need Rust installed with cargo cli available
+```
+#from the root of this project execute:
+cargo run 
+```
+
+The example in this program in [main.rs](src/main.rs), is very simple with two functions. It will run a queue example 
+and a stack example and print out the common methods to the console. 
+
--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,87 @@
 use std::collections::VecDeque;
-use std::{thread, time::Duration};
-
 
 fn main() {
-    let mut deq = VecDeque::from([-1, 0, 1]);
-    deq.push_front(-2);
-    //println!("{:?}", deq);
+    queue_example();
+    println!("\n\n\n\n\n");
     stack_example();
 }
 
-fn stack_example () {
+//FIFO Queue example
+fn queue_example() {
+    println!("Queue example with VecDeque:");
+    let mut queue = VecDeque::new();
+    queue.push_back(1);
+    queue.push_back(2);
+    queue.push_back(3);
+    queue.push_back(4);
+
+    println!("current queue: {:?} \n", queue);
+    println!("Now pop_back() twice:\n");
+
+    queue.pop_back();
+    queue.pop_back();
+
+    println!("current queue: {:?} \n", queue);
+
+    println!("queue peek -> queue.get(queue.len()-1): {:?} \n", queue.get(queue.len() - 1));
+
+    println!("queue.clear() to remove all elements from the queue");
+    queue.clear();
+
+    println!("queue.isEmpty(): {:?} \n", queue.is_empty());
+
+}
+
+//LIFO Stack example
+fn stack_example() {
     println!("Stack example with VecDeque:");
-    println!("What is VecDeque? It's a double ended queue (so works as a stack or queue) \
-    You can push and pop from front or back of the data structure.\n\n");
 
     println!("Declare Empty mutable stack: let mut stack = VecDeque::new();");
     let mut stack = VecDeque::new();
     println!("Empty Stack: {:?} \n", stack);
-    sleep();
 
     println!("push some elements onto the stack:");
 
     println!("stack.push_front(1);");
     stack.push_front(1);
-
-    sleep();
+    
     println!("current stack: {:?} \n", stack);
 
     println!("stack.push_front(2);");
     stack.push_front(2);
 
-    sleep();
+    
     println!("current stack: {:?} \n", stack);
 
     println!("stack.push_front(3);");
     stack.push_front(3);
 
-    sleep();
+    
     println!("current stack: {:?} \n", stack);
 
     println!("stack.push_front(4);");
     stack.push_front(4);
 
-    sleep();
+    
     println!("current stack: {:?} \n", stack);
 
     println!("pop an elements off the stack stack.pop_front() returns -> {:?}", stack.pop_front());
-    sleep();
+    
     println!("current stack: {:?}\n", stack);
 
     println!("pop an elements off the stack stack.pop_front() returns -> {:?}", stack.pop_front());
-    sleep();
+    
     println!("current stack: {:?}\n", stack);
 
     println!("peek at the top of the stack stack.get(0) returns -> {:?}", stack.get(0));
-    sleep();
+    
     println!("current stack (has no changes): {:?}\n", stack);
 
     println!("clear the stack with: deque.clear();");
     stack.clear();
-    sleep();
+    
     println!("current stack: {:?}\n", stack);
 
     println!("check if stack is empty with stack.is_empty(); -> {:?}", stack.is_empty());
 
-}
-
-fn sleep() { // pause so console output can be followed more easily.
-    thread::sleep(Duration::from_millis(3000));
 }
